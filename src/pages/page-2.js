1e8daf79932153ec79857ef62f9960f2077a3f9f
Brandon Chen <brandonchenjiacheng@gmail.com>
2020-07-21 17:54:27 -0700

    added netlify contact form to page-2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,33 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <form
+      method="post"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+      name="contact"
+    >
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <label>
+        Name
+        <input type="text" name="name" id="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" id="email" />
+      </label>
+      <label>
+        Subject
+        <input type="text" name="subject" id="subject" />
+      </label>
+      <label>
+        Message
+        <textarea name="message" id="message" rows="5" />
+      </label>
+      <button type="submit">Send</button>
+      <input type="reset" value="Clear" />
+    </form>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
