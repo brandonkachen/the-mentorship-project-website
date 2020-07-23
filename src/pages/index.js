5429f982ceca667a330442cd6f3444bd06616d57
Brandon Chen <brandonchenjiacheng@gmail.com>
2020-07-22 23:40:21 -0700

    added tiles for different sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,43 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hello!</h1>
-    <p>
-      <span role="img" aria-label="Construction Sign">
-        🚧🚧🚧
-      </span>
-      We're under construction right now, please check back soon.
-      <span role="img" aria-label="Construction Sign">
-        🚧🚧🚧
-      </span>
-    </p>
-    <form
+    <div class="tile is-ancestor">
+      <div class="tile is-vertical">
+        <div class="tile">
+          <div class="tile is-parent is-vertical">
+            <article class="tile is-child notification is-primary">
+              <p class="title">What do we do?</p>
+              <p class="subtitle">Top tile</p>
+            </article>
+            <article class="tile is-child notification is-warning">
+              <p class="title">How do we do mentorship?</p>
+              <p class="subtitle">Bottom tile</p>
+            </article>
+          </div>
+          <div class="tile is-parent">
+            <article class="tile is-child notification is-info">
+              <p class="title">Who we serve</p>
+              <p class="subtitle">With an image</p>
+            </article>
+          </div>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child notification is-danger">
+            <p class="title">Our Value</p>
+            <p class="subtitle">Aligned with the right tile</p>
+            <div class="content">
+              <p>stuff</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+    {/* <form
       method="post"
       netlify-honeypot="bot-field"
       data-netlify="true"
@@ -33,7 +55,7 @@ const IndexPage = () => (
         <input type="email" name="email" id="email" />
       </label>
       <button type="submit">Send</button>
-    </form>
+    </form> */}
   </Layout>
 )
 
