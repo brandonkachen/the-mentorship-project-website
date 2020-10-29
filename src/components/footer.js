2337e32568382d50f374d2e7d2e01cacc17e1345
Brandon Chen <brandonchenjiacheng@gmail.com>
2020-10-29 09:51:04 -0700

    footer bug fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,14 +8,16 @@ const Footer = ({ tabs }) => {
       <section className="hero is-small">
         <div className="hero-body">{tabs}</div>
       </section>
-      <div class="notification is-primary">
-        <div className="container has-text-centered">
-          Applications for Fall 2020 are live!<br></br>
-          <Link to="fall-2020" rel="noreferrer" target="_blank">
-            <b>Learn more and apply here.</b>
-          </Link>
+      {tabs ? (
+        <div class="notification is-primary">
+          <div className="container has-text-centered">
+            Applications for Fall 2020 are live!<br></br>
+            <Link to="fall-2020" rel="noreferrer" target="_blank">
+              <b>Learn more and apply here.</b>
+            </Link>
+          </div>
         </div>
-      </div>
+      ) : null}
       <div className="tile is-ancestor">
         <div className="tile is-parent">
           <div className="tile is-child is-1" />
